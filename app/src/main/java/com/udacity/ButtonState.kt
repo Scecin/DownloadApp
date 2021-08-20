a99f32772a74f4cb496d12de0ad62a997dcfc02b
SILVIA-CECIN <silvia.cecin@hotmail.com>
2021-08-20 18:20:59 +0100

    button state annotation Commit

--- app/src/main/java/com/udacity/ButtonState.kt
+++ app/src/main/java/com/udacity/ButtonState.kt
@@ -1,6 +1,6 @@
 package com.udacity
 
-// Button state
+// Button state with the text in any state
 sealed class ButtonState(var buttonState : Int) {
     object Clicked : ButtonState(R.string.download)
     object Loading : ButtonState(R.string.loading)
